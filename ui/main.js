15adca1757a04ba1c2acac331e6296e452ae3b8c
aghorian <anuj.ssk91@gmail.com>
2016-10-01 20:52:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,13 +3,21 @@ var counter = 0;
 var button = document.getElementById('counter');
 button.onclick = function() {
    
-    //Make a request to counter endpoint
+    //Create the request object
+    var request = new XMLHttpRequest();
     
-    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttprequest.DONE) {
+            if(request.status == 200) {
+                var counter = request.responseText;
+               // counter = counter + 1;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
+    // Make the request
+    request.open('GET','https://aghorian.imad.hasura-app.io');
+    request.send(null);
 };
