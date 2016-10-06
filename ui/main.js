7b8e14fbf30c4c717f80fbb0eea6fc47c43db642
aghorian <anuj.ssk91@gmail.com>
2016-10-07 00:13:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,20 +22,37 @@ button.onclick = function() {
     request.send(null);
 };
 
-// submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     // make a request to the server with the name entered on the screen and send it too
     // Capture a list of names and render it as a list
     
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i < names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
+    //Create the request object
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0; i < names.length; i++) {
+               list += '<li>' + names[i] + '</li>';
+      }
     
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;  
+    }
+   }
+ };
+    
+// submit name
+  var nameInput = document.getElementById('name');
+  var name      = nameInput.value;
+    // Make the request
+    request.open('GET','http://aghorian.imad.hasura-app.io/submit-name?name=' +name,true);
+    request.send(null);
+    
 };
